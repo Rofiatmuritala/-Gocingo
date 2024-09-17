ddb3f375074588457348d59045ecf98bb3162fe9
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-09-17 11:16:15 +0000

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,260 +1,118 @@
-"use client";
-
-import { useState } from "react";
 import {
-  Dialog,
-  DialogPanel,
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Popover,
-  PopoverButton,
-  PopoverGroup,
-  PopoverPanel,
 } from "@headlessui/react";
-import {
-  ArrowPathIcon,
-  Bars3Icon,
-  ChartPieIcon,
-  CursorArrowRaysIcon,
-  FingerPrintIcon,
-  SquaresPlusIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  PhoneIcon,
-  PlayCircleIcon,
-} from "@heroicons/react/20/solid";
-import { Link } from "react-router-dom";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const Contacts = [
-  {
-    name: "Get In Touch",
-    description: "Get a better understanding of your traffic",
-    to: "/contact",
-    icon: ChartPieIcon,
-  },
-  {
-    name: "FAQs",
-    description: "Make all enquiries",
-    to: "/faqs",
-    icon: CursorArrowRaysIcon,
-  },
-];
-const callsToAction = [
-  // { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-  // { name: "Contact sales", href: "#", icon: PhoneIcon },
-];
+import { Link, useLocation } from "react-router-dom";
 
-export default function Navbar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Navbar1() {
+  // Get the current url
+  const { pathname } = useLocation();
+
+  const navigation = [
+    { name: "Home", href: "/", current: pathname === "/" },
+    { name: "About Us", href: "/about", current: pathname === "/about" },
+    { name: "Projects", href: "/projects", current: pathname === "/projects" },
+    {
+      name: "Get Involve",
+      href: "/volunteering",
+      current: pathname === "/volunteering",
+    },
+    { name: "Contact", href: "/contact", current: pathname === "/contact" },
+  ];
 
   return (
-    <div className="bg-white mt-2">
-      <header className="absolute inset-x-0 top-0 z-50 ">
-        <nav
-          aria-label="Global"
-          className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8 shadow-md"
-        >
-          <div className="flex lg:flex-1 font-signature text-5xl text-green-500">
-            <Link to={"/"} className="-m-1.5 p-1.5">
-              Gocingo
-              {/* <span className="sr-only ">Gocingo</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              /> */}
-            </Link>
-          </div>
-          <div className="flex lg:hidden">
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(true)}
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="h-6 w-6" />
-            </button>
-          </div>
-          <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-            <Link
-              to={"/"}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Home
-            </Link>
-            <Link
-              to={"/about"}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              About Us
-            </Link>
-            <Link
-              to={"/projects"}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Projects
-            </Link>
-            <Link
-              to={"/volunteering"}
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Get Involve
-            </Link>
-
-            <Popover className="relative">
-              <PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                Contact
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="h-5 w-5 flex-none text-gray-400"
-                />
-              </PopoverButton>
-
-              <PopoverPanel
-                transition
-                className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <div className="p-4">
-                  {Contacts.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon
-                          aria-hidden="true"
-                          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
-                        />
-                      </div>
-                      <div className="flex-auto">
-                        <Link
-                          to={item.to}
-                          className="block font-semibold text-gray-900"
-                        >
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </Link>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
+    <Disclosure as="nav" className="bg-white border-b-2 border-gray-800">
+      {({ close }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block h-6 w-6 group-data-[open]:hidden"
+                  />
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="hidden h-6 w-6 group-data-[open]:block"
+                  />
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center md:justify-between sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <Link to={"/"} className="text-xl md:text-3xl text-green-500">
+                    Gocingo
+                  </Link>
                 </div>
-                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                  {callsToAction.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                    >
-                      <item.icon
-                        aria-hidden="true"
-                        className="h-5 w-5 flex-none text-gray-400"
-                      />
-                      {item.name}
-                    </a>
-                  ))}
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        aria-current={item.current ? "page" : undefined}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "rounded-md px-3 py-2 text-sm font-medium"
+                        )}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
                 </div>
-              </PopoverPanel>
-            </Popover>
-          </PopoverGroup>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link to={"/donate"}>
-              <button className="animate-bounce focus:animate-none hover:animate-none w-full text-left block px-4 py-2 text-base rounded-full bg-black font-medium text-white hover:text-gray-800 hover:bg-green-600">
-                Donate Now
-              </button>
-            </Link>
-          </div>
-        </nav>
-        <Dialog
-          open={mobileMenuOpen}
-          onClose={setMobileMenuOpen}
-          className="lg:hidden"
-        >
-          <div className="fixed inset-0 z-10" />
-          <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-            <div className="flex items-center justify-between font-signature text-green-500">
-              <Link to={"/"} className="-m-1.5 p-1.5">
-                Gocingo
-                {/* <span className="sr-only ">Gocingo</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              /> */}
-              </Link>
-              <button
-                type="button"
-                onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Close menu</span>
-                <XMarkIcon aria-hidden="true" className="h-6 w-6" />
-              </button>
-            </div>
-            <div className="mt-6 flow-root">
-              <div className="-my-6 divide-y divide-gray-500/10">
-                <div className="space-y-2 py-6">
-                  <Link
-                    to={"/"}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Home
-                  </Link>
-                  <Link
-                    to={"/about"}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    About Us
-                  </Link>
-                  <Link
-                    to={"/projects"}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Projects
-                  </Link>
-                  <Link
-                    to={"/volunteering"}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    Get Involve
-                  </Link>
-                  <Disclosure as="div" className="-mx-3">
-                    <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                      Contact
-                      <ChevronDownIcon
-                        aria-hidden="true"
-                        className="h-5 w-5 flex-none group-data-[open]:rotate-180"
-                      />
-                    </DisclosureButton>
-                    <DisclosurePanel className="mt-2 space-y-2">
-                      {[...Contacts, ...callsToAction].map((item) => (
-                        <DisclosureButton
-                          key={item.name}
-                          as="Link"
-                          to={item.to}
-                          className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                          {item.name}
-                        </DisclosureButton>
-                      ))}
-                    </DisclosurePanel>
-                  </Disclosure>
-                </div>
-                <div className="py-6">
-                  <Link to={"/donate"}>
+                <div className="hidden absolute inset-y-0 right-0 md:flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <Link to="/donate">
                     <button className="animate-bounce focus:animate-none hover:animate-none w-full text-left block px-4 py-2 text-base rounded-full bg-black font-medium text-white hover:text-gray-800 hover:bg-green-600">
                       Donate Now
                     </button>
                   </Link>
                 </div>
               </div>
+              <div className="md:hidden absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <Link to="/donate">
+                  <button className="animate-bounce focus:animate-none hover:animate-none w-full text-left block px-4 py-2 text-base rounded-full bg-black font-medium text-white hover:text-gray-800 hover:bg-green-600">
+                    Donate Now
+                  </button>
+                </Link>
+              </div>
             </div>
-          </DialogPanel>
-        </Dialog>
-      </header>
-    </div>
+          </div>
+
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <Link
+                  onClick={() => close()}
+                  key={item.name}
+                  to={item.href}
+                  aria-current={item.current ? "page" : undefined}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </>
+      )}
+    </Disclosure>
   );
 }
